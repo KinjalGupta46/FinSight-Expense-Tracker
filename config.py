0886a77490a42b932912bcdc7760e05f1f435006
KinjalGupta46 <kinjalgupta038@gmail.com>
2026-09-18 23:46:31 +0530

    Update config for deployment

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY = "finsight_secret_key"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev_secret_key")
 
-    MYSQL_HOST = "localhost"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = "Kinjal1234"
-    MYSQL_DB = "finsight"
+    MYSQL_HOST = os.environ.get("MYSQL_HOST", "localhost")
+    MYSQL_USER = os.environ.get("MYSQL_USER", "root")
+    MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "")
+    MYSQL_DB = os.environ.get("MYSQL_DB", "finsight")
